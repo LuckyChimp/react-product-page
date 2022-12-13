6c2028b3e087a0721591eb0d1e610d28707deb79
LuckyChimp <lukas.hartwich00@gmail.com>
2022-12-13 17:03:12 +0100

    Adjust lightbox behaviour to responsiveness

--- src/components/Lightbox.jsx
+++ src/components/Lightbox.jsx
@@ -1,6 +1,11 @@
 import ImagePreview from './ImagePreview';
+import scssVariables from '../_variables.scss';
 
 const Lightbox = ({ visible, imageId, closeLightbox }) => {
+	// only allow the Lightbox to show up, if the ImagePreview is not large enough (so, only in desktop and landscpae and not in smartphone mode)
+	const portraitMode = window.innerWidth < parseInt(String(scssVariables.breakpoint_landscape).replace('px', ''));
+	visible = visible && !portraitMode;
+
 	return (
 		<div
 			className='lightbox'
