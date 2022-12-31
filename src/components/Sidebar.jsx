01eb502cfb180de43c74980f1b554ca62c71c804
LuckyChimp <lukas.hartwich00@gmail.com>
2022-12-31 16:31:11 +0100

    Implement gray body overlay when nav sidebar opens

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,12 +2,14 @@ import { ReactComponent as CloseIcon } from '../assets/images/icon-close.svg';
 
 const Sidebar = ({ visible, NavLinks, hideSidebar }) => {
 	return (
-		<div className={visible ? 'nav-sidebar nav-sidebar--active' : 'nav-sidebar'}>
-			<CloseIcon
-				className='nav-sidebar-close-icon'
-				onClick={() => hideSidebar()}
-			/>
-			<NavLinks />
+		<div className={visible ? 'nav-sidebar-container nav-sidebar-container--active' : 'nav-sidebar-container'}>
+			<div className='nav-sidebar'>
+				<CloseIcon
+					className='nav-sidebar-close-icon'
+					onClick={() => hideSidebar()}
+				/>
+				<NavLinks />
+			</div>
 		</div>
 	);
 };
